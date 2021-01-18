9dd4bd9507947fa46945d2eb2f1f85900bc00c97
beforeisland <beforeisland@gmail.com>
2021-01-18 19:54:51 +0900

    add: テキスト2周目4-16

---
+++ html/mycakeapp/src/Model/Table/PeopleTable.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\Table;
+
+class PeopleTable extends Table
+{
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('people');
+        $this->setDisplayField('mail');
+        $this->setPrimaryKey('id');
+    }
+
+    public function findMe(Query $query, array $options)
+    {
+        $me = $options['me'];
+        return $query->where(['name like' => '%' . $me . '%'])
+            ->orWhere(['mail llike' => '%' . $me . '%'])
+            ->order(['age' => 'asc']);
+    }
+
+    public function findByAge(Query $query, array $options)
+    {
+        return $query->order(['age' => 'asc'])->order(['name' => 'asc']);
+    }
+}
